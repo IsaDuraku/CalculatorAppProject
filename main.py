0d335ce5a32377d853e4fae26e921376e9f8ea48
IsaDuraku <isaduraku3@gmail.com>
2023-09-14 17:33:06 +0200

    Second Part

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ root = Tk()
 display = Entry(root)
 display.grid(row=1,columnspan=6)
 
+#Creating 9 number buttons
 counter = 1
 for x in range(3):
     for y in range(3):
@@ -14,4 +15,14 @@ for x in range(3):
 
 button = Button(root,text="0",width=3,height=2)
 button.grid(row=5,column=1)
+
+count = 0
+operations = ['+','-',"*","/","*3.14","%","(","**",")","**2"]
+
+for x in range(4):
+    for y in range(3):
+        if count<len(operations):
+            button = Button(root,text=operations[count],width=3,height=2)
+            count+=1
+            button.grid(row=x + 2, column=y + 3)
 root.mainloop()
